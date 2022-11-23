2669328b1e206499e1afe2fd1fd80fb1292cd675
joel <joelkalema63@gmail.com>
2022-11-24 00:01:54 +0200

    add the about page

---
+++ components/about.js
@@ -0,0 +1,11 @@
+export default function About (){
+    return (
+        <div>
+            <div>
+            </div>
+            <div>
+                <h1>About me</h1>
+            </div>
+        </div>
+    )
+}
