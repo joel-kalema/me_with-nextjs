487afbe74d63c59f88704761f74eaf2b7ef254ab
joel <joelkalema63@gmail.com>
2022-11-29 12:51:44 +0200

    fix cards

--- components/sckills.js
+++ components/sckills.js
@@ -8,7 +8,7 @@ export default function Skills (){
                 <h1 className="text-[#ffd175] font-bold text-2xl">LANGUAGES AND FRAMEWORKS</h1>
                 <div className="flex flex-wrap mt-6 justify-between">
                     {langages.map((item) => (
-                        <p className="w-1/5 flex mt-6"><span className="text-[#ffd175] mr-2 text-2xl">{item.icons}</span>{item.langages}</p>
+                        <p className="w-1/5 flex mt-6 text-xs"><span className="text-[#ffd175] mr-2 text-2xl">{item.icons}</span>{item.langages}</p>
                     ))}
                 </div>
             </div>
