b24be78c01c26ee210bdaab62e2d9e347998fa09
joel <joelkalema63@gmail.com>
2022-12-08 23:58:16 +0200

    style popup

--- components/data/portfolio.js
+++ components/data/portfolio.js
@@ -2,24 +2,24 @@ export const projects = [
     {
         id: 1,
         image: '/images/live.jpg',
-        title: 'this is the title',
-        details: 'this project was made to help people',
-        technologies: ['React', 'Scss'],
+        title: 'Global-warming',
+        details: 'This is a responsive website that navigates from the home page to the about page, following mobile first rule.',
+        technologies: ['Html', 'Css', 'Js'],
         live: '#'
     },
     {
         id: 2,
-        image: '/images/live.jpg',
-        title: 'this is the title',
-        details: 'this project was made to help people',
+        image: '/images/todo.jpg',
+        title: 'To-Do-list',
+        details: 'The To_Do_List is a project designed to help you get organized, achieve your goals and never forget a thing. By keeping record of your plans and allowing you to edit them the way you want.',
         technologies: ['React', 'Scss'],
         live: '#'
     },
     {
         id: 3,
-        image: '/images/live.jpg',
-        title: 'this is the title',
-        details: 'this project was made to help people',
+        image: '/images/movi.jpg',
+        title: 'SHOWNET',
+        details: 'A movie database website that displays all the information about a show including the discription and the rating of the show. You can search for a show, like and also comment on any of your favourite show. This project consumed TVmaze api.',
         technologies: ['React', 'Scss'],
         live: '#'
     },
