c2d3aec3cf20c9efb0d9c10ba6bff295b761ec45
joel <joelkalema63@gmail.com>
2022-12-27 01:05:34 +0200

    fix about paddings

--- components/about.js
+++ components/about.js
@@ -9,7 +9,7 @@ export default function About (){
     }, []);
 
     return (
-        <div className='bg-[#161922] py-40 px-8 lg:px-60 md:px-24' data-aos="fade-up" data-aos-duration="1000">
+        <div className='bg-[#161922] py-10 md:py-40 px-8 lg:px-60 md:px-24' data-aos="fade-up" data-aos-duration="1000">
             <div className="flex flex-col md:flex-row">
                 <div className="lg:w-2/4 w-full text-center md:text-start md:w-2/5 lg:2/4"  data-aos="fade-up" data-aos-duration="1000">
                     <h1 className="text-[11rem] font-bold">3<span className="text-[#ffd175] animate-ping">.</span></h1>
@@ -18,7 +18,7 @@ export default function About (){
                         <h2>Experience</h2> 
                     </div>
                 </div>
-                <div className="w-2/4 md:w-3/5 lg:2/4"  data-aos="fade-up" data-aos-duration="1000">
+                <div className="w-full mt-10 md:mt-0 md:w-3/5 lg:2/4 text-center md:text-start "  data-aos="fade-up" data-aos-duration="1000">
                     <h1 className="text-3xl text-[#ffd175]">About me</h1>
                     <p className="my-8 text-sm">I'm a software developer specialized 
                         in different technologies, Building web applications, 
