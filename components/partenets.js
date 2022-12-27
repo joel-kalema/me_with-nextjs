65a058aa86f3580aadf07903b79fb9d4b491d418
joel <joelkalema63@gmail.com>
2022-12-27 11:25:17 +0200

    fix contact's media queries

--- components/partenets.js
+++ components/partenets.js
@@ -14,14 +14,14 @@ export default function Partenets (){
             <h2>Have any Project in Mind?</h2>
             <h1 className='text-5xl text-[#ffd175] text-center my-6'>Just Say Hello!</h1>
             <div className="flex flex-col md:flex-row justify-between mt-20">
-                <div className="w-full md:w-2/4 text-xl" data-aos="fade-up" data-aos-duration="1000">
+                <div className="w-full md:w-1/4 lg:w-2/4 text-sm lg:text-xl" data-aos="fade-up" data-aos-duration="1000">
                     <p>Let's make somthing new, different and more meanigful 
                         or make think more visual or Conceptual?
                         <span className="text-[#ffd175]">Just say Hello!</span>
                     </p>
                 </div>
                 <p className='text-[#ffd175] hidden md:flex text-xl mt-16 ml-[-2rem]'><AiOutlineDash /><TbArrowNarrowRight /></p>
-                <div className="w-full md:w-2/5 bg-[#1b212f] p-12 mb-[-13rem] shadow-xl" data-aos="fade-up" data-aos-duration="2000">
+                <div className="w-full md:w-3/5 lg:w-2/5 bg-[#1b212f] p-12 mb-[-13rem] shadow-xl" data-aos="fade-up" data-aos-duration="2000">
                     <form>
                         <input type="text" name="userName" id="name" placeholder="Name" maxlength="30" required /><br />
                         <input type="email" name="email" id="email" placeholder="Email" required className="mt-8"/><br />
