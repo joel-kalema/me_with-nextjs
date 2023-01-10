baf6c6df399bccd19a1c100d32090f4e1b616bc1
joel <joelkalema63@gmail.com>
2023-01-10 23:54:50 +0200

    fix sociaux margins

--- components/header.js
+++ components/header.js
@@ -23,7 +23,7 @@ export default function Header () {
                         <div className='flex'>
                             <h1 className='flex atems-center absolute'>
                                 <p className='text-xl mr-2'>MENU</p>
-                                <AiOutlineCloseCircle className='text-2xl mr-2'/>
+                                <AiOutlineCloseCircle className='text-2xl'/>
                             </h1>
                             <div className='flex items-center flex-col mr-16'>
                                 <div className='w-6 h-6 border border-yellow-200 mb-8'></div>
