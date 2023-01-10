0faa2ef1ccde7feee7ebaf73cfb680799c3852a0
joel <joelkalema63@gmail.com>
2023-01-10 23:37:04 +0200

    fix sociaux margins

--- components/header.js
+++ components/header.js
@@ -40,7 +40,7 @@ export default function Header () {
                                 <p className='flex items-center mb-2'><span className='mr-2'><CiMail /></span> joelkalema63@gmail.com</p>
                                 <p className='flex items-center mb-2 w-full'><span className='mr-2'><TbPhoneCalling /></span> +250 (0) 789-816-723</p>
                             </div>
-                            <div className='flex w-full md:w-2/4 mt-4'>
+                            <div className='flex w-full md:w-3/4 mt-4'>
                                 <a href='https://twitter.com/JoelJklm'><CiTwitter className='mr-4 hover:text-[#ffd175]'/></a>
                                 <a href='https://github.com/joel-kalema'><FiGithub className='mr-4 hover:text-[#ffd175]'/></a>
                                 <a href='https://www.linkedin.com/in/joel-kalema/'><FiLinkedin className='mr-4 hover:text-[#ffd175]'/></a>
