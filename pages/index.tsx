3961589b745fcd0548e1c46fa0722e2dd5577b85
joel <joelkalema63@gmail.com>
2023-02-07 22:25:58 +0200

    add motion to the header

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,14 @@ import Portfolio from '../components/portfolio';
 import Partenets from '../components/partenets';
 import Infos from '../components/infos'
 import Footer from '../components/footer'
+import { motion, useScroll } from "framer-motion"
 
 
 export default function Home() {
+    const { scrollYProgress } = useScroll();
     return (
         <div className={`${styles.container}`}>
+            <motion.div style={{ scaleX: scrollYProgress }} className=".progress-bar" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}/>  
             <Header /> 
             <HomePage />
             <About />
