e720fcbe82893f2dea97f298415a59027530ec8a
joel <joelkalema63@gmail.com>
2023-03-26 02:00:56 +0200

    Updated the style

--- pages/projects/[name].tsx
+++ pages/projects/[name].tsx
@@ -18,7 +18,7 @@ export default function Project (){
             <div className="flex justify-center items-center h-screen relative">
             <div>
                 <Link href='/#portfolio' className="fixed flex items-center top-10 z-50 left-10 hover:text-[#ffd175]">
-                    <MdOutlineBackspace className='mr-2'/>PORTFOLIO
+                    <MdOutlineBackspace className='mr-2'/>
                 </Link>
                 <div className='flex  w-1/8 lg:ml-[-4rem] justify-between absolute top-10 right-20'>
                     <a href='https://twitter.com/JoelJklm'><CiTwitter className='mr-4 hover:text-[#ffd175]'/></a>
@@ -27,7 +27,7 @@ export default function Project (){
                     <a href='https://angel.co/u/joel-kalema'><FaAngellist className='hover:text-[#ffd175]'/></a>
                 </div>
             </div>
-            <motion.div className="flex-col flex lg:flex-row py-20 px-10 lg:px-60 drop-shadow-xl detail">
+            <div className="flex-col flex lg:flex-row py-20 px-10 lg:px-60 drop-shadow-xl detail">
                 <img src={project?.image} alt='images' className="w-full lg:w-3/5 shadow-2xl"/>
                 <div className="mt-6 lg:mt-0 lg:ml-6 relative">
                     <h1 className="font-bold text-3xl mt-8 ml-6 text-[#ffd175]">{project?.title}</h1>
@@ -39,11 +39,11 @@ export default function Project (){
                          >{lang}</p>))}
                     </div>
                     <div className="flex text-sm text-[#ffd175]">
-                        <a className="flex items-center cursor-pointer mr-4">Source Code<AiFillGithub className="ml-1"/></a>
-                        <a className="flex items-center cursor-pointer">See Live<MdOutlineDirections className="ml-1"/></a>
+                        <a href={project?.live} className="flex items-center cursor-pointer mr-4">See Live<MdOutlineDirections className="ml-1"/></a>
+                        <a href={project?.source} className="flex items-center cursor-pointer">Source Code<AiFillGithub className="ml-1"/></a>
                     </div>
                 </div>
-            </motion.div>
+            </div>
         </div>
         <div className='py-10 text-center'> 
             <p className="text-xs">© 2022 <a href="https://www.linkedin.com/in/joel-kalema/">Joel Kalema</a>, All right reserved</p>
