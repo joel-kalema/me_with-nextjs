abec671bff8a8a273478c63750fb44e85b1d7fc1
joel <joelkalema63@gmail.com>
2023-03-26 02:04:46 +0200

    update the content

--- components/partenets.js
+++ components/partenets.js
@@ -12,12 +12,12 @@ export default function Partenets (){
     return(
         <div id='contact' className="flex justify-center flex-col items-center px-10 md:px-20 lg:px-60 pt-20" data-aos="fade-up" data-aos-duration="1000">
             <h2>Have any Project in Mind?</h2>
-            <h1 className='text-5xl text-[#ffd175] text-center my-6'>Just Say Hello!</h1>
-            <div className="flex flex-col md:flex-row justify-between mt-20">
-                <div className="w-full md:w-1/4 lg:w-2/4 text-sm text-center md:text-start lg:text-xl" data-aos="fade-up" data-aos-duration="1000">
+            <h1 className='text-5xl text-[#ffd175] text-center my-6'>Get in Touch!</h1>
+            <div className="flex flex-col md:flex-row justify-between mt-4 md:mt-20">
+                <div className="w-full md:w-1/4 lg:w-2/4 text-[1rem] text-center md:text-start lg:text-xl" data-aos="fade-up" data-aos-duration="1000">
                     <p>Let's make somthing new, different and more meanigful 
                         or make think more visual or Conceptual?
-                        <span className="text-[#ffd175]">Just say Hello!</span>
+                        <span className="text-[#ffd175]"> Get in Touch!</span>
                     </p>
                 </div>
                 <p className='text-[#ffd175] hidden md:flex text-xl mt-16 ml-[-2rem]'><AiOutlineDash /><TbArrowNarrowRight /></p>
