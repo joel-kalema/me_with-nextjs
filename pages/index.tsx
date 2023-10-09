5f35a715fc70a220bea12f15b5f02e9c2ffd489c
joel-kalema <joelkalema63@gmail.com>
2023-10-09 19:58:17 +0200

    import comps

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Portfolio from '../components/portfolio';
 import Partenets from '../components/partenets';
 import Infos from '../components/infos'
 import Footer from '../components/footer'
+// import Recomandation from '../components/recommendations'
 import { motion, useScroll } from "framer-motion"
 
 
@@ -19,6 +20,7 @@ export default function Home() {
             <HomePage />
             <About />
             <Area />
+            {/* <Recomandation /> */}
             <Portfolio />
             <Partenets />
             <Infos />
