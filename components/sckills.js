195fe2273bb7a79d1cd324edaea8c648f1909c6b
joel-kalema <joelkalema63@gmail.com>
2023-11-15 22:23:28 +0100

    updated responsiveness

--- components/sckills.js
+++ components/sckills.js
@@ -9,12 +9,12 @@ export default function Skills (){
     }, []);
 
     return(
-        <div className="w-4/4 bg-[#1b212f] py-6 md:py-8 lg:py-14 px-4 md:px-8 lg:px-16 mt-20 mb-[-15rem] shadow-xl mx-auto"   data-aos="fade-up" data-aos-duration="1000">
+        <div className="w-4/4 bg-[#1b212f] py-6 md:py-8 lg:py-14 px-4 md:px-20 lg:px-16 mt-20 mb-[-15rem] shadow-xl mx-auto"   data-aos="fade-up" data-aos-duration="1000">
             <div className="w-full">
                 <h1 className="text-[#ffd175] font-bold text-sm md:text-2xl">LANGUAGES AND FRAMEWORKS</h1>
                 <div className="flex flex-wrap mt-6">
                     {langages.map((item) => (
-                        <p key={item.langages} className="w-2/4 md:w-1/5 flex mt-6 text-xs"><span className="text-[#ffd175] mr-2 text-2xl">{item.icons}</span>{item.langages}</p>
+                        <p key={item.langages} className="w-2/4 md:w-1/5 flex mt-6 text-xs md:text-sm"><span className="text-[#ffd175] mr-2 text-xs md:text-2xl">{item.icons}</span>{item.langages}</p>
                     ))}
                 </div>
             </div>
